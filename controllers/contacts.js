09f21e2fee477c3f2de5dec7b534e1e606cb8da7
kimlah <71352287+kimlah@users.noreply.github.com>
2023-01-20 12:05:54 -0800

    update # returned with requests

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -81,7 +81,7 @@ const deleteContact = async (req, res) => {
     .deleteOne({_id: userId}, true);
   console.log(response);
   if (response.deletedCount > 0) {
-    res.status(204).send();
+    res.status(200).send();
   }
   else {
     res.status(500).json(response.error || 'An error occurred while deleting the contact');
