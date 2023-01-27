095daa5fccb5543053019f507bc453979e44c4e1
kimlah <71352287+kimlah@users.noreply.github.com>
2023-01-27 15:15:35 -0800

    change to https in swagger.js

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'My Contacts API',
   },
   host: 'w02assignment.onrender.com',
-  schemes: ['http'],
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
